dbccbbd3b6efee889e9181f4ce6f836e1cffc75e
Eric <ericpaulbishop@gmail.com>
2012-05-13 15:55:34 -0400

    need all runlevels

--- debian_install.sh
+++ debian_install.sh
@@ -4,7 +4,9 @@ apt-get install tor
 cp ./torrc /etc/tor/torrc
 cp torify.init /etc/init.d/torify
 chmod 755 /etc/init.d/torify
-ln -s /etc/init.d/torify /etc/rc3.d/S40torify
+for rcn in 2 3 4 5 ; do
+	ln -s /etc/init.d/torify /etc/rc${rcn}.d/S40torify
+done
 update-rc.d torify enable
 
 
