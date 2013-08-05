9d640a2e440dc6b13b3a3ad4243d67b54a136697
Eric <ericpaulbishop@gmail.com>
2013-08-04 22:04:50 -0400

    correct init script provides, be a bit less verbose, simplify debian install

--- debian_install.sh
+++ debian_install.sh
@@ -4,9 +4,10 @@ apt-get install tor
 cp ./torrc /etc/tor/torrc
 cp torify.init /etc/init.d/torify
 chmod 755 /etc/init.d/torify
-for rcn in 2 3 4 5 ; do
-	ln -s /etc/init.d/torify /etc/rc${rcn}.d/S40torify
-done
+update-rc.d torify defaults
 update-rc.d torify enable
+
+/etc/init.d/tor restart
+/etc/init.d/torify restart
 
 
