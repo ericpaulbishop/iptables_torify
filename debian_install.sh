09096ffa3ce83aafe2512ce1e00b3e7eb98fd5e8
Eric <ericpaulbishop@gmail.com>
2013-08-04 22:21:53 -0400

    edit torrc to have defaults specified by debian, so only port parameters are new/different

--- debian_install.sh
+++ debian_install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 apt-get install tor
+mkdir -p /etc/tor/
 cp ./torrc /etc/tor/torrc
+chmod 644 /etc/tor/torrc
 cp torify.init /etc/init.d/torify
 chmod 755 /etc/init.d/torify
 update-rc.d torify defaults
